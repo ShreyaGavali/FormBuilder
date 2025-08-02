3c76e99674e05fdd4b66089461bee1a418573de5
Shreya Gavali <shreyagavali@gmail.com>
2025-08-02 13:29:32 +0530

    issue fixed

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://form-builder-tau-seven.vercel.app'],
   credentials: true,
 }));
 
